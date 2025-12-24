c45c71397b1c2e6f04650425abcd51b2e0b38703
mysterypaint <83567173+mysterypaintwo@users.noreply.github.com>
2025-12-24 08:42:15 -0800

    update: newgame.asm is now reliant on a global pointer table

--- src/newgame.asm
+++ src/newgame.asm
@@ -1,73 +1,110 @@
 ; =======================================================
 ; Set Inventory + Starting location at the start of the game
 ; =======================================================
-!RandoStartTable_energy = 99
-!RandoStartTable_missiles = 15
-!RandoStartTable_powerBombs = 5
-!RandoStartTable_reserves = 0
-!RandoStartTable_items = 5
-!RandoStartTable_beams = 5
-!RandoStartTable_area = 1
-!RandoStartTable_load = 0
+org $8A80D2
+  JSL NewGameSetup
+  BRA $00
 
 org !START_FREESPACE_SLOT_05
 RandoStartTable:
-  .energy
-    dw $0063
-  .missiles
-    dw $0005
-  .powerBombs
-    dw $0000
-  .reserves
-    dw $0001
-  .items
-    dw $0000
-  .beams
-    dw $0000
-  .area
-    db $0001
-  .load
-    db $0000
+  dw $0063   ; energy
+  dw $0005   ; missiles
+  dw $0000   ; power bombs
+  dw $0001   ; reserves
+  dw $0000   ; items
+  dw $0000   ; beams
+  db $01     ; area
+  db $00     ; load
 
+; =======================================================
+; Set Inventory + Starting location at the start of the game
+; Pointer-driven
+; =======================================================
 NewGameSetup:
 {
   PHB
   PHK
   PLB
-  LDA #!RandoStartTable_energy
+
+  ; ---------------------------
+  ; Load RandoStartTable pointer
+  ; ---------------------------
+  REP #$20            ; 16-bit A
+  LDA.l RandoStartTablePtr
+  STA $00
+  LDA.l RandoStartTablePtr+1
+  STA $01
+  SEP #$20
+  LDA.l RandoStartTablePtr+2
+  STA $02
+
+  REP #$20            ; 16-bit A/Y
+  LDY #$0000
+
+  ; energy
+  LDA [$00],y
   STA $09C2
   STA $09C4
-  LDA #!RandoStartTable_missiles
+  INY #2
+
+  ; missiles
+  LDA [$00],y
   STA $09C6
   STA $09C8
-  LDA #!RandoStartTable_powerBombs
+  INY #2
+
+  ; power bombs
+  LDA [$00],y
   STA $09CE
   STA $09D0
-  LDA #!RandoStartTable_reserves
+  INY #2
+
+  ; reserves
+  LDA [$00],y
   STA $09D4
   STA $09EC
   STA $09EE
-  LDA #!RandoStartTable_items
+  INY #2
+
+  ; items
+  LDA [$00],y
   STA $09A2
   STA $09A4
-  LDA #!RandoStartTable_beams
+  INY #2
+
+  ; beams
+  LDA [$00],y
   STA $09A6
   STA $09A8
-  LDA #!RandoStartTable_area
+  INY #2
+
+  ; ---------------------------
+  ; 8-bit values
+  ; ---------------------------
+  SEP #$20            ; 8-bit A
+
+  ; area
+  LDA [$00],y
   STA $079F
-  LDA #!RandoStartTable_load
+  INY
+
+  ; load / station
+  LDA [$00],y
   STA $078B
 
-  ; events and fixes
-  SEP #$20
+  ; ---------------------------
+  ; Events and fixes
+  ; ---------------------------
   LDA #$FF : STA $7ED909
   LDA #$01 : STA $0DF4
-  REP #$20
 
+  REP #$20
   JSL EventsEnableMap
+
   PLB
   RTL
 }
+
 
 ; =======================================================
 ; Enable map fully on new game
