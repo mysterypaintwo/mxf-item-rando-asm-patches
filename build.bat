c1c596926ee891e10e222134c968e4a18811fc6f
mysterypaint <83567173+mysterypaintwo@users.noreply.github.com>
2025-12-24 07:16:56 -0800

    update: output IPS, not BPS

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 copy /Y baserom.sfc out.sfc
 %~dp0/asar/asar.exe %~dp0/src/main.asm out.sfc
-%~dp0/flips/flips.exe --create baserom.sfc out.sfc mxf-item-rando.bps
+%~dp0/flips/flips.exe -c -i baserom.sfc out.sfc mxf-item-rando-patches.ips
