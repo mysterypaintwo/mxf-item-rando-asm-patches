72f2cd2605c81a31b1e13a4c1074d91488240d41
mysterypaint <83567173+mysterypaintwo@users.noreply.github.com>
2025-12-24 07:26:33 -0800

    update: setup a (hopefully-consistent?!) pointer table

--- src/main.asm
+++ src/main.asm
@@ -3,4 +3,8 @@ incsrc macros.asm
 incsrc freespace.asm
 incsrc newgame.asm
 
+org !START_FREESPACE_SLOT_00    ; $80FE88 ; PC 0x007E88
+RandoStartTablePtr:
+  dl RandoStartTable
+
 %printfreespace_all()
